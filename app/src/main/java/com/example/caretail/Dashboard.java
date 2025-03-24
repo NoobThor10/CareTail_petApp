ae558ad579c93568ea215bc74f87aa4c1bc95ac7
Lav-18 <lavanyashah708@gmail.com>
2025-03-24 22:52:42 +0530

    Init

--- app/src/main/java/com/example/caretail/Dashboard.java
+++ app/src/main/java/com/example/caretail/Dashboard.java
@@ -2,7 +2,11 @@ package com.example.caretail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        ImageButton healthRecords=findViewById(R.id.healthRecords);
+        healthRecords.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Dashboard.this,HealthRecords.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
