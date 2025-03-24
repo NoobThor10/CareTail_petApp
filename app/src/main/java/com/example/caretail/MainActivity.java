e9eeb6b32d5ad54ce70eb21fed77cf9df91f5f64
yashs <yashsamsukha10@gmail.com>
2025-03-24 18:22:04 +0530

    init

--- app/src/main/java/com/example/caretail/MainActivity.java
+++ app/src/main/java/com/example/caretail/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.caretail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView logo=findViewById(R.id.imageView);
+
+        // Fade-in animation
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setDuration(2000); // 2 seconds
+        logo.startAnimation(fadeIn);
+
+        // Move to LoginActivity after 3 seconds
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this,Login.class);
+            startActivity(intent);
+            finish(); // Close the splash screen
+        }, 3000); // 3 seconds delay
     }
 }
