eeb281f338d3daeb4c79cd41fa59e53b825fc78c
yashs <yashsamsukha10@gmail.com>
2025-03-24 20:24:27 +0530

    init

--- app/src/main/java/com/example/caretail/Login.java
+++ app/src/main/java/com/example/caretail/Login.java
@@ -2,13 +2,27 @@ package com.example.caretail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button login=findViewById(R.id.login);
+       login.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent=new Intent(Login.this,Dashboard.class);
+               startActivity(intent);
+               finish();
+           }
+       });
     }
 }
