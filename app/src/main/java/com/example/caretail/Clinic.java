836e7dd4a641fd2a9e85aba0f73ba7989a8c0ba3
Lav-18 <lavanyashah708@gmail.com>
2025-04-12 07:58:14 +0530

    Init

--- app/src/main/java/com/example/caretail/Clinic.java
+++ app/src/main/java/com/example/caretail/Clinic.java
@@ -1,6 +1,9 @@
 package com.example.caretail;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,19 @@ public class Clinic extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_clinic);
+        ImageButton back=findViewById(R.id.btn_back);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Clinic.this, DietPlan.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
